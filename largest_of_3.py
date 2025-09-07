ee6d1d593572439a1341927b6895516cbfef7221
Biruh1 <biruhalegesse@gmail.com>
2025-09-07 00:13:01 -0700

    Added largest of 3 program using nested if statements

---
+++ largest_of_3.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if num1 >= num2:
+    if num1 >= num3:
+        largest = num1
+    else:
+        largest = num3
+else:
+    if num2 >= num3:
+        largest = num2
+    else:
+        largest = num3
+
+print("You entered:", num1, num2, num3)
+print("Largest number is:", largest)
